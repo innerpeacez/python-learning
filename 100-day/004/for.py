a2767aaaaf075298f8cca9f805e5c860f557068d
innerpeacez <zhaihw0417@163.com>
2019-08-12 15:28:17 +0800

    004 for-in while

---
+++ 100-day/004/for.py
@@ -0,0 +1,12 @@
+sum_number = 0
+
+"""
+range(101)可以产生一个0到100的整数序列。
+range(1, 100)可以产生一个1到99的整数序列。
+range(1, 100, 2)可以产生一个1到99的奇数序列，其中的2是步长，即数值序列的增量。
+"""
+
+for num in range(101):
+    sum_number += num
+
+print(sum_number)
