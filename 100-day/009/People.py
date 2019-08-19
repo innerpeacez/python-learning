75c02c03b60645979afd23695e451c2322d68800
innerpeacez <zhaihw0417@163.com>
2019-08-19 16:15:04 +0800

    009 python 面向对象 封装 继承 多态

---
+++ 100-day/009/People.py
@@ -0,0 +1,27 @@
+class People(object):
+    """人"""
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    def play(self):
+        print('%s正在愉快的玩耍.' % self._name)
+
+    def watch_av(self):
+        if self._age >= 18:
+            print('%s正在观看爱情动作片.' % self._name)
+        else:
+            print('%s只能观看《熊出没》.' % self._name)
